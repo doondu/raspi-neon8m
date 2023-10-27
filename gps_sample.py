abd30093e49d50b8f6d53c78836d3f7b529d7f4b
YoonSoo Kim <rabbit7183@gmail.com>
2023-10-28 01:21:11 +0900

    Add pyubx2 library

--- gps_sample.py
+++ gps_sample.py
@@ -1,34 +1,37 @@
 import serial
 import datetime
+from pyubx2 import UBXReader
 
 #Change UART port number
-ser = serial.Serial("/dev/cu.usbserial-0001", baudrate=115200, timeout=0.001)
-ser.flushInput()
-ser.flushOutput()
+stream = serial.Serial("/dev/ttyACM0", baudrate=115200, timeout=0.001)
+# ser.flushInput()
+# ser.flushOutput()
+ubr = UBXReader(stream)
 idx = 0
-
-nmea_data = b""
+# nmea_data = b""
 
 # skip first line, since it could be incomplete
-ser.readline()
+# ser.readline()
 
 while True:
-    while ser.inWaiting():
-        idx += 1
-        nmea_sentence = ser.readline()
-        # nmea_data += nmea_sentence
+    (raw_data, parsed_data) = ubr.read()
+    print(parsed_data)
+    # while ser.inWaiting():
+    #     idx += 1
+    #     # nmea_sentence = ser.readline()
+    #     # nmea_data += nmea_sentence
 
-        print(f"{idx}: \n {nmea_sentence}")
+    #     print(f"{idx}: \n {}")
 
-        # if idx % 100 == 0:
-        #     print(f"idx: {idx}")
+    #     # if idx % 100 == 0:
+    #     #     print(f"idx: {idx}")
             
-        # if idx % 2000 == 0:
+    #     # if idx % 2000 == 0:
 
-        #     # save to file after 2000 sentences added
-        #     filename = datetime.datetime.utcnow().strftime("data/gps_data_%Y%m%d-%H%M%S.nmea")
-        #     f = open(filename, "ab")
-        #     f.write(nmea_data)
-        #     f.close()
+    #     #     # save to file after 2000 sentences added
+    #     #     filename = datetime.datetime.utcnow().strftime("data/gps_data_%Y%m%d-%H%M%S.nmea")
+    #     #     f = open(filename, "ab")
+    #     #     f.write(nmea_data)
+    #     #     f.close()
             
-        #     nmea_data = b""
+    #     #     nmea_data = b""
